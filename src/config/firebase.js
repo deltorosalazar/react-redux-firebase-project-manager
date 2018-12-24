24e0f949aa96f78022c196f4e56b173e2f046d5b
Manuel F. Del Toro <deltorosalazar@gmail.com>
2018-12-24 13:23:38 -0500

    dotenv dependency added and firebase credentials hidden

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,16 @@
+require('dotenv').config()
 import firebase from 'firebase/app'
 import 'firebse/firestore'
 import 'firebse/auth'
 
 var config = {
-  apiKey: "AIzaSyCK7cdElx_D7-omFJtFQC8W2F3iut_Ndk0",
-  authDomain: "react-project-manager-3892a.firebaseapp.com",
-  databaseURL: "https://react-project-manager-3892a.firebaseio.com",
-  projectId: "react-project-manager-3892a",
-  storageBucket: "react-project-manager-3892a.appspot.com",
-  messagingSenderId: "1081065101905"
-};
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+}
 
 firebase.initializeApp(config)
 firebase.firestore().settings({
